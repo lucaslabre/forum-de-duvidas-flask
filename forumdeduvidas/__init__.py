f37921c3ad82f3d1fcc82a9ef3d9a9c9952be1ab
Lucas Labre <48772886+lucaslabre@users.noreply.github.com>
2024-12-27 15:03:27 -0300

    Configuração do banco de dados Postgres do railway

--- forumdeduvidas/__init__.py
+++ forumdeduvidas/__init__.py
@@ -2,11 +2,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '322209975e1d48ffa38481152ac73ed2'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///forum.db'
+if os.getenv('DATABASE_URL'):
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:WJqlPyjvqOsaUjrQZpXWSmpXrFCwHrNl@postgres.railway.internal:5432/railway'
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///forum.db'
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
